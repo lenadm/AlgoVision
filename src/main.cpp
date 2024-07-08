20f6121d35b2f58be1af8192c89ccb8c016530c1
lena <lenarichards2234@gmail.com>
2024-07-08 22:25:21 +0100

    rendered array to framebuffer for future writing

--- src/main.cpp
+++ src/main.cpp
@@ -10,48 +10,41 @@ const Color accents = {0x36, 0x41, 0x56, 0xff};
 const Color selectedForeground = {0x91, 0x20, 0x20, 0xff};
 const Color selectedAccents = {0xba, 0x18, 0x18, 0xff};
 
-void DrawNumberedRectangles(char *letter, Rectangle rectangle, int offset, int fontSize, Color foreground, Color accents);
-void DrawInit(int *numbers);
+const int screenHeight = 800;
+const int screenWidth = 800;
 
 int main(void) {
+	Rectangle gridSquare = {80, 80, 64, 64};
 	InitWindow(800, 800, "raylib test");
+	RenderTexture2D base = LoadRenderTexture(640, 64);
 	SetTargetFPS(144);
 	srand((unsigned) time(NULL));
 
-	int numbers[10];
+	char numbers[10];
 	for (int i = 0; i < 10; i++) {
-		numbers[i] = rand() % 10;
+		numbers[i] = char('0' + rand() % 10);
 	}
 
-	DrawInit(numbers);
-	while (!WindowShouldClose()) {
-		DrawInit(numbers);
-		sleep(1);
-	}
-}
-
-void DrawNumberedRectangles(char *letter, Rectangle rectangle, int offset, int fontSize, Color foreground, Color accents) {
-	int boxCenter = ((rectangle.width - fontSize) / 2);
-	int textCenter = (offset + boxCenter );
-
-	DrawRectangle(rectangle.x + offset, rectangle.y,
-	rectangle.width, rectangle.height, foreground);
-
-	DrawText(letter, rectangle.x + textCenter, rectangle.y + boxCenter,
-	fontSize, accents);
-
-	DrawRectangleLines(rectangle.x + offset, rectangle.y,
-	rectangle.width, rectangle.height, accents);
-}
-
-void DrawInit(int *numbers) {
-	BeginDrawing();
+	BeginTextureMode(base);
 	ClearBackground(background);
-	Rectangle gridSquare = {80, 80, 64, 64};
 	for (int i = 0; i < 10; i++) {
 		int offset = i * 64;
-		char letter = numbers[i] + '0';
-		DrawNumberedRectangles(&letter, gridSquare, offset, 24, foreground, accents);
+		char letter[2] = {numbers[i], '\0'};
+		int boxCenter = ((gridSquare.width - 24) / 2);
+		int textCenter = (offset + boxCenter );
+
+		DrawRectangle(0 + offset, 0, gridSquare.width, gridSquare.height, foreground);
+
+		DrawText(letter, textCenter, 20, 24, accents);
+
+		DrawRectangleLines(0 + offset, 0, gridSquare.width, gridSquare.height, accents);
 	}
-	EndDrawing();
+	EndTextureMode();
+
+	while (!WindowShouldClose()) {
+		BeginDrawing();
+		DrawTextureRec(base.texture, (Rectangle) {0, 0, (float)base.texture.width, (float)-base.texture.height}, (Vector2) {80, 80}, WHITE);
+		EndDrawing();
+	}
 }
+
