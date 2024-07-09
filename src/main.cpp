59fb80566ae1251e8f805f24625222e642d2d772
lena <lenarichards2234@gmail.com>
2024-07-09 16:27:33 +0100

    bubble sort is done

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <ctime>
 #include <iostream>
 #include <unistd.h>
+#include <thread>
 
 const Color background = {0x11, 0x15, 0x1c, 0xff};
 const Color foreground = {0x21, 0x2d, 0x40, 0xff};
@@ -13,6 +14,7 @@ const int screenHeight = 800;
 const int screenWidth = 800;
 
 void DrawNumberedRectangles(Rectangle gridSquare, char *letter, int offset, Color primary, Color secondary);
+void DrawThread(char *numbers, Rectangle gridSquare);
 
 int main(void) {
 	Rectangle gridSquare = {80, 80, 64, 64};
@@ -25,27 +27,39 @@ int main(void) {
 		numbers[i] = char('0' + rand() % 10);
 	}
 
+	DrawThread(numbers, gridSquare);
 	while (!WindowShouldClose()) {
-		for (int first = 0; first < 9; first++) {
-			for (int second = 0; second < 9; second++) {
-				if (numbers[second] > numbers[second + 1]) {
-					char tmp = numbers[second];
-					numbers[second] = numbers[second + 1];
-					numbers[second + 1] = tmp;
+		std::thread draw(DrawThread, numbers, gridSquare);
+		draw.join();
+	}
+}
+
+void DrawThread(char *numbers, Rectangle gridSquare) {
+	for (int first = 0; first < 9; first++) {
+		bool isSorted = true;
+		for (int second = 0; second < 9; second++) {
+			BeginDrawing();
+			for (int box = 0; box < 10; box++) {
+				int offset = box * 64;
+				char letter[2] = {numbers[box], '\0'};
+				if (box == second || box == second + 1) {
+					DrawNumberedRectangles(gridSquare, letter, offset, selectedForeground, accents);
+				} else {
+					DrawNumberedRectangles(gridSquare, letter, offset, foreground, accents);
 				}
-				BeginDrawing();
-				for (int box = 0; box < 10; box++) {
-					int offset = box * 64;
-					char letter[2] = {numbers[box], '\0'};
-					if (box == second || box == second + 1) {
-						DrawNumberedRectangles(gridSquare, letter, offset, selectedForeground, accents);
-					} else {
-						DrawNumberedRectangles(gridSquare, letter, offset, foreground, accents);
-					}
-				}
-				EndDrawing();
-				sleep(1);
 			}
+			EndDrawing();
+
+			if (numbers[second] > numbers[second + 1]) {
+				isSorted = false;
+				char tmp = numbers[second];
+				numbers[second] = numbers[second + 1];
+				numbers[second + 1] = tmp;
+			}
+			sleep(1);
+		}
+		if (isSorted == true) {
+			exit(0);
 		}
 	}
 }
